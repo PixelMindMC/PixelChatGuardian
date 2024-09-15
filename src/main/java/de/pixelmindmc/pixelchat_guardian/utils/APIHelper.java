a6346100b4b717339e684aa33d46014b98f1fd2a
ToothyDev <chosenfate@gmx.de>
2024-09-15 19:59:02 +0200

    Deprecated URL ersetzen

--- src/main/java/de/pixelmindmc/pixelchat_guardian/utils/APIHelper.java
+++ src/main/java/de/pixelmindmc/pixelchat_guardian/utils/APIHelper.java
@@ -14,6 +14,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
+import java.net.URI;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.Map;
@@ -35,7 +36,7 @@ public class APIHelper {
     }
 
     public String makeApiCall(String prompt) throws Exception {
-        URL url = new URL(api_url);
+        URL url = new URI(apiUrl).toURL();
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.setRequestMethod("POST");
         connection.setRequestProperty("Content-Type", "application/json");
