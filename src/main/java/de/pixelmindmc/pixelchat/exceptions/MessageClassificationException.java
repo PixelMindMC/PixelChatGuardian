fb35333b02499af284fb94dafb37542206cc6522
Gaming12846 <zockmann@gmail.com>
2024-09-21 00:11:31 +0200

    Add copyright

--- src/main/java/de/pixelmindmc/pixelchat/exceptions/MessageClassificationException.java
+++ src/main/java/de/pixelmindmc/pixelchat/exceptions/MessageClassificationException.java
@@ -1,3 +1,8 @@
+/*
+ * This file is part of PixelChat Guardian.
+ * Copyright (C) 2024 PixelMindMC
+ */
+
 package de.pixelmindmc.pixelchat.exceptions;
 
 public class MessageClassificationException extends Exception {
