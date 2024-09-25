0391f4e58ee3edec1a0dc68ce31d1160f6605e24
ToothyDev <chosenfate@gmx.de>
2024-09-25 21:22:43 +0200

    Remove now-unused key since actions were removed

--- src/main/java/de/pixelmindmc/pixelchat/constants/APIConstants.java
+++ src/main/java/de/pixelmindmc/pixelchat/constants/APIConstants.java
@@ -8,7 +8,6 @@ package de.pixelmindmc.pixelchat.constants;
 public class APIConstants {
     public static final String BLOCK_KEY = "block";
     public static final String REASON_KEY = "reason";
-    public static final String ACTION_KEY = "action";
     public static final String CONTENT_KEY = "content";
 
     public static final String GITHUB_RELEASES_URL = "https://api.github.com/repos/PixelMindMC/PixelChatGuardian/releases/latest";
