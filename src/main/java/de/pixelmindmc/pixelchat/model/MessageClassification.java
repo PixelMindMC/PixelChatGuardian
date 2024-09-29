e5cd9fae3884a59feb9040d535db26ed3740990b
ToothyDev <chosenfate@gmx.de>
2024-09-29 19:52:36 +0200

    Add documentation to MessageClassification

--- src/main/java/de/pixelmindmc/pixelchat/model/MessageClassification.java
+++ src/main/java/de/pixelmindmc/pixelchat/model/MessageClassification.java
@@ -5,5 +5,11 @@
 
 package de.pixelmindmc.pixelchat.model;
 
+/**
+ * Represents the AI-Classification of a message
+ *
+ * @param block  Whether the message should be blocked or not
+ * @param reason The reason why the message should be blocked
+ */
 public record MessageClassification(boolean block, String reason) {
 }
