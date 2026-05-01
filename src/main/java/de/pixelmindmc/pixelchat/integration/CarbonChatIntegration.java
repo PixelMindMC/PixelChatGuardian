f0ed5f165e098434d3b5030ee76562641971e8d5
Gaming12846 <36011371+Gaming12846@users.noreply.github.com>
2026-05-02 01:02:20 +0200

    Update copyright

--- src/main/java/de/pixelmindmc/pixelchat/integration/CarbonChatIntegration.java
+++ src/main/java/de/pixelmindmc/pixelchat/integration/CarbonChatIntegration.java
@@ -1,6 +1,18 @@
 /*
+ * Copyright (C) 2024 - 2026 PixelMindMC
+ *
  * This file is part of PixelChat Guardian.
- * Copyright (C) 2026 PixelMindMC
+ *
+ * PixelChat Guardian is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * PixelChat Guardian is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with PixelChat Guardian.
+ * If not, see <https://www.gnu.org/licenses/>.
  */
 
 package de.pixelmindmc.pixelchat.integration;
@@ -97,7 +109,7 @@ public class CarbonChatIntegration {
 
         // Check if classification matches any enabled blocking rules
         if (chatGuardHelper.messageMatchesEnabledRule(classification)) {
-            boolean blockOrCensor = configHelper.getString(ConfigConstants.ChatGuard.MESSAGE_HANDLING).equals("BLOCK");
+            boolean blockOrCensor = "BLOCK".equals(configHelper.getString(ConfigConstants.ChatGuard.MESSAGE_HANDLING));
             if (blockOrCensor) {
                 event.cancelled(true);
             } else {
